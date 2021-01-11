06f7d7b510aa7b63de101e5c16ffb7f00fd56537
김수진 <sjkim910115@gmail.com>
2021-01-11 18:04:28 +0900

    react test

---
+++ propsChild.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PropsChild = (props) => {
+    return (
+        <View>
+            <Text onPress={props.cState}>
+                {props.sText}
+            </Text>
+        </View>
+    )
+}
+export default PropsChild;
